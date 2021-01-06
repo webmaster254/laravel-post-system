8a3a68571a0a94c48d25451cd01c3b5cc3f563fa
Joseph Macharia <josephm280@gmail.com>
2021-01-06 14:17:20 -0800

    created postry and project setup

---
+++ postry/resources/views/layouts/app.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <title>Posty</title>
+</head>
+<body class="bg-gray-100">
+<nav class="p-6 bg-white flex justify-between">
+   <ul class="flex itms-center">
+        <li>
+            <a href="" class="p-3">Home</a>
+        </li>
+        <li>
+            <a href="" class="p-3">Dashboard</a>
+        </li>
+        <li>
+            <a href="" class="p-3">Post</a>
+        </li>
+   </ul>
+
+   <ul class="flex itms-center">
+        <li>
+            <a href="" class="p-3">Joseph</a>
+        </li>
+        <li>
+            <a href="" class="p-3">Login</a>
+        </li>
+        <li>
+            <a href="" class="p-3">Register</a>
+        </li>
+        <li>
+            <a href="" class="p-3">Login</a>
+        </li>
+   </ul>
+</nav>
+    @yield('content')
+</body>
+</html>
